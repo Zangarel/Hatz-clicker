5889aa49d3f4d8388bc9e0a60b82e9ac48519472
Cristian Ticu <cristi.ticu1@gmail.com>
2023-02-20 23:04:12 +0200

    Modificat logica scenelor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,20 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    SceneLogic sceneLogic;
-    // Start is called before the first frame update
+    public static float timer;
+    public static readonly int GameSceneIndex = 1;
+    public static readonly int SplashSceneIndex = 0;
+    public int CurrentSceneIndex = 0;
+
+    void Awake()
+    {
+        
+    }
     void Start()
     {
-        sceneLogic = gameObject.AddComponent<SceneLogic>();
-        sceneLogic.LoadScene(sceneIndex: SceneLogic.SplashSceneIndex);
+        DontDestroyOnLoad(gameObject);
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(CurrentSceneIndex == SplashSceneIndex) {
+            if (timer >= 3)
+            {
+                LoadScene(GameSceneIndex);
+                CurrentSceneIndex = GameSceneIndex;
+                timer = 0;
+            }
+            else
+                timer += Time.deltaTime;
+        }
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 }
