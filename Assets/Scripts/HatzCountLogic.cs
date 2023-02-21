0765efc957dfcbaa45cc0e72249e7efca2f7af7d
Cristian Ticu <cristi.ticu1@gmail.com>
2023-02-21 15:35:16 +0200

    StartUpScene, Particles

---
+++ Assets/Scripts/HatzCountLogic.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class HatzCountLogic : MonoBehaviour
+{
+    public GameObject mainLogic;
+    public Text hatzCount;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string textToSet = mainLogic.GetComponent<MainLogicScript>().GetHatzCount().ToString();
+        textToSet += "Hatzuri";
+        hatzCount.text = textToSet;
+    }
+}
---
+++ Assets/Scripts/HatzParticleLogic.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatzParticleLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public double hatzCount;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setHatzCount(double hatz)
+    {
+        hatzCount = hatz;
+    }
+}
